38f301768826f26ba3013ea41eb45442e956ff72
emirkannn <115951309+emirkannn@users.noreply.github.com>
2023-08-24 10:06:40 +0300

    Dotnet Print Capture
    The output time was shortened. Ironpdf and itext library used together

---
+++ yazici2/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace yazici2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.yazdir = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // yazdir
+            // 
+            this.yazdir.Location = new System.Drawing.Point(457, 163);
+            this.yazdir.Name = "yazdir";
+            this.yazdir.Size = new System.Drawing.Size(102, 61);
+            this.yazdir.TabIndex = 0;
+            this.yazdir.Text = "Yazdir";
+            this.yazdir.UseVisualStyleBackColor = true;
+            this.yazdir.Click += new System.EventHandler(this.yazdir_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.yazdir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button yazdir;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ yazici2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using iText.Html2pdf;
+using iText.Kernel.Pdf;
+using iText.Layout;
+
+namespace yazici2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void yazdir_Click(object sender, EventArgs e)
+        {
+            // HTML dosyasını seçme işlemi
+            
+            string mutlakYol = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "kantar.html");
+
+            // Dosyadaki HTML içeriğini okuma
+            string htmlContent = File.ReadAllText(mutlakYol);
+
+            // HTML içeriğini PDF dosyasına dönüştür
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileName = "kantar.pdf";
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "  " + saveFileDialog.FileName;
+
+            DialogResult result = MessageBox.Show("Çıktı almak istiyor musunuz?", "Print Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            try
+            {
+                if (result == DialogResult.Yes)
+
+                {
+                    string pdfFilePath = saveFileDialog.InitialDirectory;
+                    
+                    ConvertHtmlToPdf(htmlContent, pdfFilePath);
+
+                    // PDF dosyasını yazdır
+                    PrintPdf(pdfFilePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Yazdırma işlemi sırasında bir hata oluştu. \n" + ex.ToString(), "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ConvertHtmlToPdf(string htmlContent, string pdfFilePath)
+        {
+
+
+            // PDF dosyasını oluşturmak ve içeriğini doldurmak için FileStream oluşturulur.
+            using (var stream = new FileStream(pdfFilePath, FileMode.Create))
+            {
+                // PDF yazıcısı oluşturulur ve FileStream'e bağlanır.
+                var writer = new PdfWriter(stream);
+
+                // PDF belgesi oluşturulur ve yazıcıya bağlanır.
+                var pdf = new PdfDocument(writer);
+
+                // PDF belgesinin düzenini oluşturmak için Document nesnesi oluşturulur.
+                var document = new Document(pdf);
+
+                // HTML içeriğini PDF dosyasına çevirmek için iText HtmlConverter kullanılır.
+                HtmlConverter.ConvertToPdf(htmlContent, pdf, new ConverterProperties());
+
+                // PDF belgesi kapatılır, kaynakları serbest bırakılır.
+                document.Close();
+            }
+
+                MessageBox.Show("PDF başarıyla oluşturuldu: " + pdfFilePath, "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void PrintPdf(string pdfFilePath)
+        {
+
+            var pdf = IronPdf.PdfDocument.FromFile(pdfFilePath);
+            pdf.Print(100);
+
+            
+                
+            MessageBox.Show("PDF başarıyla yazıcıya gönderildi", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+
